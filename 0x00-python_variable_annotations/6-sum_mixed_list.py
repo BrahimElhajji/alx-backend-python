94bd8a825f2a1be0411b2016d10b4168720b0bb3
BrahimElhajji <elhajji080898@gmail.com>
2024-08-06 15:34:40 -0700

    fixing pycodstyle

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -6,14 +6,15 @@ of a list containing both integers and floats.
 
 from typing import Union, List
 
+
 def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """
     Calculates the sum of a list containing both integers and floats.
-    
+
     Args:
         mxd_lst (List[Union[int, float]]):
         A list containing integers and/or float values.
-    
+
     Returns:
         float: The sum of the values in the list, as a float.
     """
--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -6,17 +6,18 @@ and a number to a tuple.
 
 from typing import Union, Tuple
 
+
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """
     Converts a string and a number to a tuple with the string
     and the square of the number.
-    
+
     Args:
         k (str): A string value.
-        v (Union[int, float]): An integer or float value to be squared.
-    
+        v (Unon[int, float]): An integer or float value to be squared.
+
     Returns:
-        Tuple[str, float]: A tuple where the first element is the string 
+        Tuple[str, float]: A tuple where the first element is the string
         and the second element is the square of the number.
     """
     return (k, v * v)
